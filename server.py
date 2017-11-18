b932bc83bcf4002d7b9ad861d48ce81127ea3f8b
Arush Goyal <arushgyl@gmail.com>
2017-11-18 22:26:43 +0530

    vedant

---
+++ server.py
@@ -0,0 +1,37 @@
+import flask
+import flask_shelve
+import shelve
+from flask import request
+import json
+
+
+with shelve.open('shelve.db') as db:
+    with open('initData.json') as f:
+        mydict = json.load(f)
+        db.update(mydict)
+
+
+app = flask.Flask(__name__)
+app.config['SHELVE_FILENAME'] = 'shelve.db'
+flask_shelve.init_app(app)
+
+
+@app.route('/monkey_detection', methods=['GET', 'POST'])
+def md():
+    db = flask_shelve.get_shelve('c', )
+
+    camera_details = db[request.form['camera']]
+    camera_details['status'] = request.form['status']
+    db[request.form['camera']] = camera_details
+
+    return 'OK'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    db = flask_shelve.get_shelve('c')
+    return json.dumps(dict(db))
+
+
+if __name__ == '__main__':
+    app.run()
